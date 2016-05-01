2eddd7b77981f1779dbdadefc862d64664132bac
Kostas Minaidis <kostas@creativecollective.gr>
2016-05-01 13:19:44 +0200

    First take on Windows auto compile

--- plugins/sublime-text-autocompiler/buildmammouthonsave.py
+++ plugins/sublime-text-autocompiler/buildmammouthonsave.py
@@ -1,7 +1,7 @@
 # REQUIREMENTS: npm install -g mammouth
 # USAGE: Files must have a .mammouth or .mmt suffix
 
-import sublime, sublime_plugin,os
+import sublime, sublime_plugin,os,sys
 from os.path import dirname, realpath
 
 class BuildMammouthOnSave(sublime_plugin.EventListener):
@@ -9,9 +9,14 @@ class BuildMammouthOnSave(sublime_plugin.EventListener):
   def on_post_save(self, view):
     mammouthFile = view.file_name()
     filename, file_extension = os.path.splitext(mammouthFile)
+    if sublime.platform() == "windows":
+      file_extension = file_extension.encode(sys.getfilesystemencoding())
     if file_extension == ".mammouth" or file_extension == ".mmt":
       print("Compiling: " + mammouthFile)
-      view.window().run_command('exec',{'cmd': ["/usr/local/bin/mammouth", "-c", mammouthFile] })
+      if sublime.platform() == "windows":
+        os.system("mammouth -c " + mammouthFile)
+      else:
+        view.window().run_command('exec',{'cmd': ["/usr/local/bin/mammouth", "-c", mammouthFile] })
 
 # REFERENCES
 # http://www.purplebeanie.com/Development/automatically-run-build-on-save-in-sublime-text-2.html
